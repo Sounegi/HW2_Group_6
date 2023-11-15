190ff95286471318ef851e11b4dc720ef59eabcd
kevin <k.richardsonhalim2002@gmail.com>
2023-11-15 18:11:27 +0800

    Portal: Scene Change - Kill All Enemy Inovked

--- Assets/Scripts/Map Script/EndFlag.cs
+++ Assets/Scripts/Map Script/EndFlag.cs
@@ -5,6 +5,23 @@ using UnityEngine;
 public class EndFlag : MonoBehaviour
 {
     public bool active;
+    public EnemyController tracker;
+
+    void Start()
+    {
+        this.enabled = false;   
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<CapsuleCollider>().enabled = false;
+        // tracker = GameObject.Find("EnemyController").GetComponent<EnemyController>();
+        tracker.OnEnd += enable_portal;
+    }
+
+    public void enable_portal()
+    {
+        this.enabled = true;
+        GetComponent<MeshRenderer>().enabled = true;
+        GetComponent<CapsuleCollider>().enabled = true;
+    }
 
     void OnTriggerEnter(Collider obj)
     {
